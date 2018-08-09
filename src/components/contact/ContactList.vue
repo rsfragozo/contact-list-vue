e16525a9dcb25f3e403f965cf079518dd4997205
Rodrigo Soares Fragozo <rodrigo.fragozo@gmail.com>
2018-08-09 17:54:16 +0200

    created ContactList component and added it to App component

---
+++ src/components/contact/ContactList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="contact-list">
+      <ul>
+        <li v-for="(contact, id) in contacts" v-bind:key="id">
+          {{contact.firstname}} {{contact.lastname}}
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ContactList',
+  mounted() {
+    fetch("http://rest.learncode.academy/api/vue-5/contacts")
+      .then(response => response.json())
+      .then((data) => {
+        this.contacts = data;
+      })
+  },
+  data() {
+    return {
+      contacts: []
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
+
